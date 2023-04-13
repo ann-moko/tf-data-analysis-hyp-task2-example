db3f2d0fad45586394a76809a2debe253fc7ff2f
ann-moko <129892885+ann-moko@users.noreply.github.com>
2023-04-13 17:41:50 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,6 @@ import numpy as np
 chat_id = 371784753 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.03
+    return (stats.anderson_ksamp([x, y]).significance_level < alpha)
+    
